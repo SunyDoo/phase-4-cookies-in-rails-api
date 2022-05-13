508b3088fc6175165ffb32b7fd7e302e66570697
Sunil <snilavarath@gmail.com>
2022-05-13 10:08:19 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   
   def index
-    session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    session[:session_hello] ||= "Hello"
+    cookies[:cookies_hello] ||= "Hello"
     render json: { session: session, cookies: cookies.to_hash }
   end
 
